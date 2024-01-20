ccd1105225ebf1c654b075acc474c3cef3ba526a
Ankita Gahtori <118369642+GahtoriAnkita@users.noreply.github.com>
2024-01-20 07:50:03 +0000

    02_datatypes update

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,22 @@
+"use strict";  //treat all JS code as never version
+
+// alert(3+3); // we are using node.js not using browser
+
+console.log("Ankita");
+
+let name = "Ankita";
+let age = 18;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol  => unique
+
+
+// object 
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
