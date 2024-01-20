c3ab20db80d27b838d8551fc415fa90f965a46a6
Ankita Gahtori <118369642+GahtoriAnkita@users.noreply.github.com>
2024-01-20 08:34:34 +0000

    operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,28 +1,63 @@
 // let score = "33guk"
 // let score = null;
-let score = undefined;
+// let score = undefined;
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let chngeDatatype = Number(score);
-console.log(typeof chngeDatatype);
-console.log(chngeDatatype);
+// let chngeDatatype = Number(score);
+// console.log(typeof chngeDatatype);
+// console.log(chngeDatatype);
 
 
 //"33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = "ankita";
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = "ankita";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 //"ankita" => true
 
-let sumNumber = 33;
-let stringNumber = String(sumNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let sumNumber = 33;
+// let stringNumber = String(sumNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ********************************* Operations ****************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2-2);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = "hitesh";
+
+let str3 = str1 + " " + str2;
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2;
+console.log(num1);
+
+let gamecounter = 100;
+gamecounter++;
+console.log(gamecounter);
+++gamecounter;
+console.log(gamecounter);
