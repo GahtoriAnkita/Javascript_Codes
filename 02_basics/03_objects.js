ff8f0e4d02837aa8556ab22d445613036bc3d1d5
Ankita Gahtori <118369642+GahtoriAnkita@users.noreply.github.com>
2024-02-04 15:56:42 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,35 @@
+// object --> declared in two ways ---> 1. Literal (isme singleton nhi hota h)  2. Constructor
+// singleton --> koi bhi constructor se banate h toh vo singleton hota h only single istances
+// object.create  ---> singleton
+
+//objects Literals
+
+const mySym = Symbol("key1");
+
+const Jsuser = {
+    //key and value is defined
+    name: "Ankita",
+    "full name": "Ankita Gahtori",
+    [mySym] : "myKey1",
+    age: 21,
+    location: "Delhi",
+    email: "abc@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"],
+}
+console.log(Jsuser.name);
+console.log(Jsuser["name"]);
+console.log(Jsuser["full name"]);
+console.log(Jsuser[mySym]);
+console.log(typeof mySym);
+
+
+Jsuser.email = "ankita@mail.com";
+Object.freeze(Jsuser);
+
+function g1(){
+    return xy
+}
+let x= g1;
+console.log(x);
+
